75f41329be0278db465697cf004961f6a0506386
CodeLelouch742 <114353572+CodeLelouch742@users.noreply.github.com>
2022-10-18 20:07:28 +0300

    Add files via upload

---
+++ lab2/Utils.h
@@ -0,0 +1,96 @@
+#ifndef LABA2_UTILS_H
+#define LABA2_UTILS_H
+
+#include <iostream>
+#include <chrono>
+#include <math.h>
+#include <Windows.h>
+
+using namespace std;
+
+float pathLenght(float, float, float, float);
+int snailsInput();
+int snailsConfusionOutput();
+void shortestTimeOutput(int);
+void lookingSnailsConfusion();
+
+
+int snailAmount=0;
+float shortestTime = 141422;//100000*sqrt(2)+1 максимальная длина по диагонали
+
+
+struct Snail
+{
+    float x = 0;
+    float y = 0;
+    int lenght = snailAmount - 1;
+    bool confusion = false;
+    float* paths = new float[lenght]();
+
+    void lookingConfusion()//O(n*log(n))
+    {
+        mergeSort(paths, 0, lenght-1);
+            if (paths[0] == paths[1])
+                this->confusion = true;
+    }
+
+    void mergeSort(float* allPaths, int first, int last)//O(nlog(n))
+    {
+
+        int center = first + (last - first) / 2;
+        if (first < last)
+        {
+            mergeSort(allPaths, first, center);
+            mergeSort(allPaths, center + 1, last);
+
+            merge(allPaths, first, center, last);//O(n)
+        }
+    }
+
+    void merge(float* partPaths, int first, int center, int last)//O(n)
+    {
+        int i = 0, j = 0, k = first;
+        int number1 = center - first + 1;
+        int number2 = last - center;
+        float* part1Paths = new float[number1]();
+        float* part2Paths = new float[number2]();
+
+        for (int i = 0; i < number1; i++)
+            part1Paths[i] = partPaths[first + i];
+        for (int j = 0; j < number2; j++)
+            part2Paths[j] = partPaths[center + 1 + j];
+
+        while (i < number1 && j < number2)
+        {
+            if (part1Paths[i] <= part2Paths[j])
+            {
+                partPaths[k] = part1Paths[i];
+                i++;
+            }
+            else
+            {
+                partPaths[k] = part2Paths[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < number1)
+        {
+            partPaths[k] = part1Paths[i];
+            i++;
+            k++;
+        }
+
+        while (j < number2)
+        {
+            partPaths[k] = part2Paths[j];
+            j++;
+            k++;
+        }
+        delete[] part1Paths;
+        delete[] part2Paths;
+    }
+};
+
+#endif //LABA2_UTILS_H
---
+++ lab2/main.cpp
@@ -0,0 +1,118 @@
+#include "Utils.h"
+
+Snail* snails;
+float temp;
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    if(snailsInput() == 1)
+    {
+        return 0;
+    }
+
+    lookingSnailsConfusion();
+
+    for (int i = 0; i < snailAmount; i++)
+    {
+        for (int j = 0; j < snailAmount; j++)
+        {
+            if (i != j)
+            {
+                temp = pathLenght(snails[i].x, snails[i].y, snails[j].x, snails[j].y);
+                if(snails[i].confusion && snails[j].confusion)
+                    temp = 141422;
+                if(!snails[i].confusion && !snails[j].confusion){
+                    temp = temp/2;
+                }
+                if (temp < shortestTime)
+                    shortestTime = temp;
+            }
+        }
+    }
+
+    int snailsNumber = snailsConfusionOutput();
+    shortestTimeOutput(snailsNumber);
+    delete [] snails;
+    system("pause");
+    return 0;
+}
+
+void lookingSnailsConfusion(){
+    int numberpath = 0;
+    for (int i = 0; i < snailAmount; i++)
+    {
+        for (int j = 0; j < snailAmount; j++)
+        {
+            if(i != j)
+            {
+                temp = pathLenght(snails[i].x, snails[i].y, snails[j].x, snails[j].y);
+                snails[i].paths[numberpath] = temp;
+                numberpath++;
+            }
+        }
+        snails[i].lookingConfusion();
+        numberpath=0;
+    }
+}
+
+int snailsConfusionOutput(){
+    int snailsNumber = 0;
+    for (int i = 0; i < snailAmount; i++)
+    {
+        if (snails[i].confusion)
+        {
+            cout << "Улитка " << i << " в замешательстве\n";
+            snailsNumber++;
+        }
+    }
+    return snailsNumber;
+}
+
+void shortestTimeOutput(int snailsNumber){
+    if (snailsNumber != snailAmount)
+        cout << "Ближайшие улитки достигнут друг друга через " << shortestTime << " сек." << endl << endl;
+}
+
+int snailsInput(){
+    int check;
+    cout << "Введите колличество улиток: ";
+    cin >> snailAmount;
+    if (snailAmount == 1) {
+        cout << "Улитка в замешательстве, так как она одна" << endl;
+        system("pause");
+        return 1;
+    }
+    snails = new Snail[snailAmount];
+    cout << "Введите стартовую позицию для каждой улитки(X,Y), где X и Y должны быть в диапозоне от 0 до 100000" << endl;
+    for (int i = 0; i < snailAmount; i++)
+    {
+        check = 1;
+        while(check){
+            cout <<"Улитка "<< i << ": " << endl << "X: ";
+            cin >> snails[i].x;
+            cout << "Y: ";
+            cin >> snails[i].y;
+            cout << endl;
+            if(((snails[i].x > 0) && (snails[i].x < 100000)) && ((snails[i].y > 0) && (snails[i].y < 100000)))
+            {
+                check=0;
+            }
+            else{
+                cout << "Введены неверные координаты. Попробуйте снова.\n";
+            }
+        }
+    }
+    return 0;
+}
+
+float pathLenght(float x1, float y1, float x2, float y2) {
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+
+
+
+
+
